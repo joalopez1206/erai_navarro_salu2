54013cc400868eba96d714156c1c3e6b918c0a1b
joalopez1206 <joalopez@ug.uchile.cl>
2023-05-04 13:47:50 -0400

    xd

--- include/merge_sort.h
+++ include/merge_sort.h
@@ -1,4 +1,5 @@
 #include <stddef.h>
 #pragma once
-void my_merge(const int *l1, const int *l2, size_t l1_len, size_t l2_len, int* out);
+void my_merge(int *l1,int *l2, size_t l1_len, size_t l2_len, int* out);
+void mergesort(int *l1, int N);
 void test_1();
